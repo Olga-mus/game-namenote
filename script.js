d739b3a58f0d70effdfe02ac40c0e7451648aa0f
Olga <olganum@gmail.com>
2023-07-08 21:29:08 +0300

    частично добавлена логика поиска верного ответа

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const items = document.querySelectorAll('.name');
-console.log(items);
+const cardNote = document.querySelectorAll('.items__image');
 const placeholders = document.querySelectorAll('.placeholder');
-console.log(placeholders);
 const names = document.querySelectorAll('.name');
 const btnReset = document.querySelector('.reset');
 
@@ -24,18 +23,52 @@ function dragend(evt) {
   evt.target.classList.remove('dragging', 'hidden');
 }
 
-placeholders.forEach((item, i) => {
+placeholders.forEach((item) => {
   //item.addEventListener('dragover', dragover);
-  // console.log(item);
-  // console.log(item.children);
+
   item.addEventListener('dragover', dragover);
   item.addEventListener('dragenter', dragenter);
   item.addEventListener('dragleave', dragleave);
+
   //item.addEventListener('drop', dragdrop);
   item.addEventListener('drop', (evt) => {
     console.log('dragdrop');
     item.append(document.querySelector('.dragging'));
     evt.target.classList.remove('hovered');
+    /////////////////////////////////////////////////////////////////////////
+    //атрибуты окно для вставки
+    let past;
+    for (const name of item.children) {
+      past = name.getAttribute('data-note'); //атрибуты окно для вставки data-note элемента внутри placholders
+    }
+    //атрибуты карточек
+    let cardAttr;
+    //цикл, который по очереди сравнит data-image в items с data-note элемента внутри placholders
+
+    for (const i of cardNote) {
+      cardAttr = i.getAttribute('data-image');
+      if (cardAttr === past) {
+        console.log(
+          '_____________________________________________________________'
+        );
+        console.log('OK');
+        console.log('атрибут карточки', cardAttr);
+        console.log('атрибут вставки', past);
+        console.log(
+          '_____________________________________________________________'
+        );
+      } else {
+      }
+      console.log(
+        '_____________________________________________________________'
+      );
+      console.log('NO');
+      console.log('атрибут карточки', cardAttr);
+      console.log('атрибут вставки', past);
+      console.log(
+        '_____________________________________________________________'
+      );
+    }
   });
 });
 
@@ -58,22 +91,8 @@ function dragleave(evt) {
 //   console.log('dragdrop');
 // }
 
-// function answerRight() {
-//   placeholders.forEach((item) => {
-//     const answer = item.dataset.answer;
-//     //console.log(item.dataset.answer);
-//     names.forEach((item) => {
-//       const note = item.dataset.note;
-//       //console.log(item.dataset.note);
-//       if (answer === note) {
-//         console.log('sdfsdfsdf');
-//       }
-//     });
-//   });
-// }
-
-// answerRight();
-
 btnReset.addEventListener('click', function () {
   window.location.reload();
 });
+
+//Реализация верного ответа: У Вас должен быть массив items с Вашими нотами и атрибутами data-image. У Вас уже есть массив placеholders с перетащеными в них карточками. Вам остается каждый раз в событии drop запустить цикл, который по очереди сравнит data-image в items с data-note элемента внутри placholders. Если все совпадают, ноты расположены верно и человеку можно сообщить о победе.
